239338e3ec08edf46d03b720de5e9a3168510a3b
Adriana <a23311067@uthermosillo.edu.mx>
2025-02-05 20:02:32 -0700

    se realizo una barra lateral

--- src/components/ListaDeTareas.vue
+++ src/components/ListaDeTareas.vue
@@ -4,7 +4,8 @@
         <input type="text" v-model="nuevaTarea" @keyup.enter="addTarea">
         <ul>
             <li v-for="(tarea, index) in tareas" :key="index">
-                    {{ tarea }}
+                {{tarea}}
+                <button @click="borrarTarea (index)">Borrar</button>
             </li>
         </ul>
     </div>
@@ -17,6 +18,9 @@ const nuevaTarea = ref('');
 const addTarea = () => {
     tareas.value.push(nuevaTarea.value);
     nuevaTarea.value = '';
+}
+const borrarTarea = (index) => {
+    tareas.value.splice(index, 1);
 }
 
 </script>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="sidebar">
+        <h2>Barra lateral</h2>
+        <ul>
+            <li><router-link to="/contador">Contador</router-link></li>
+            <li><router-link to="/ListaDeTareas">Lista de tareas</router-link></li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+.sidebar {
+    width: 200px;
+    background-color: darkgrey;
+    padding: 20px;
+}
+</style>
+
+
